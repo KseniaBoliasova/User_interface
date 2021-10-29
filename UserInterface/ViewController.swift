02d910245d63d2992dfeac81279c2aa1ca2f7579
Ksenia Boliasova <rkv2008@yandex.ru>
2021-10-29 17:32:40 +0300

    Loginform

---
+++ UserInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInterface
+//
+//  Created by Ксения Болясова on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
