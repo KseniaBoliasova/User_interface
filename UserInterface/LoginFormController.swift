3253e89bfb774188a09f43442d77e2fe62646da5
Ksenia Boliasova <rkv2008@yandex.ru>
2021-10-31 05:40:34 +0300

    Lesson 2

--- UserInterface/LoginFormController.swift
+++ UserInterface/LoginFormController.swift
@@ -13,17 +13,7 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var passwordInput: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBAction func loginButtonPressed(_ sender: Any) {
-            // Получаем текст логина
-            let login = loginInput.text!
-            // Получаем текст-пароль
-            let password = passwordInput.text!
-            
-            // Проверяем, верны ли они
-            if login == "admin" && password == "123456" {
-                print("успешная авторизация")
-            } else {
-                print("неуспешная авторизация")
-            }
+
         }
 
     
@@ -61,6 +51,43 @@ class LoginFormController: UIViewController {
             scrollView?.addGestureRecognizer(hideKeyboardGesture)
         }
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            // Проверяем данные
+            let checkResult = checkUserData()
+            
+            // Если данные не верны, покажем ошибку
+            if !checkResult {
+                showLoginError()
+            }
+            
+            // Вернем результат
+            return checkResult
+        }
+        
+        func checkUserData() -> Bool {
+            guard let login = loginInput.text,
+                let password = passwordInput.text else { return false }
+            
+            if login == "admin" && password == "123456" {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        func showLoginError() {
+            // Создаем контроллер
+            let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+            // Создаем кнопку для UIAlertController
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            // Добавляем кнопку на UIAlertController
+            alter.addAction(action)
+            // Показываем UIAlertController
+            present(alter, animated: true, completion: nil)
+        }
+
+
+
     
     override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
